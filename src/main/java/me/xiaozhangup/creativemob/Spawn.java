66ba8ab2bbe68c7b3a4c74d3b1f3826a29356815
xiaozhangup <xiaozhangshiw@163.com>
2022-01-31 23:38:01 +0800

    update

--- src/main/java/me/xiaozhangup/creativemob/Spawn.java
+++ src/main/java/me/xiaozhangup/creativemob/Spawn.java
@@ -2,11 +2,20 @@ package me.xiaozhangup.creativemob;
 
 import me.happylandmc.core.math.Number;
 import org.bukkit.Location;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.EquipmentSlot;
+
+import java.util.UUID;
 
 public class Spawn implements Listener {
 
@@ -23,9 +32,54 @@ public class Spawn implements Listener {
                     )) {
                         Entity entity = location.getWorld().spawnEntity(location , EntityType.fromName(
                                 Main.plugin.getConfig().getString("MobTable.List." + i + ".Type")
-                        ));
+                        ), CreatureSpawnEvent.SpawnReason.NATURAL);
 
-                        entity.setCustomNameVisible();
+                        entity.setCustomName(Main.plugin.getConfig().getString("MobTable.List." + i + ".Settings.Name"));
+                        entity.setCustomNameVisible(Main.plugin.getConfig().getBoolean("MobTable.List." + i + ".Settings.Keepname"));
+
+                        if(entity instanceof LivingEntity) {
+                            LivingEntity mob = (LivingEntity) entity;
+
+                            AttributeInstance attribute = mob.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+                            attribute.setBaseValue(Main.plugin.getConfig().getDouble("MobTable.List." + i + ".Settings.Health"));
+                            mob.setHealth(Main.plugin.getConfig().getDouble("MobTable.List." + i + ".Settings.Health"));
+
+                            EntityEquipment equipment = ((LivingEntity) entity).getEquipment();
+                            equipment.clear();
+                            equipment.setItemInMainHandDropChance(0);
+                            equipment.setItemInOffHandDropChance(0);
+                            equipment.setBootsDropChance(0);
+                            equipment.setChestplateDropChance(0);
+                            equipment.setHelmetDropChance(0);
+                            equipment.setLeggingsDropChance(0);
+
+                            equipment.setItemInOffHand(Main.plugin.getConfig().getItemStack(
+                                    "ItemTable." + Main.plugin.getConfig().getString(
+                                            "MobTable.List." + i + ".OffHand")
+                            ));
+                            equipment.setItemInMainHand(Main.plugin.getConfig().getItemStack(
+                                    "ItemTable." + Main.plugin.getConfig().getString(
+                                            "MobTable.List." + i + ".Hand")
+                            ));
+                            equipment.setHelmet(Main.plugin.getConfig().getItemStack(
+                                    "ItemTable." + Main.plugin.getConfig().getString(
+                                            "MobTable.List." + i + ".Head")
+                            ));
+                            equipment.setChestplate(Main.plugin.getConfig().getItemStack(
+                                    "ItemTable." + Main.plugin.getConfig().getString(
+                                            "MobTable.List." + i + ".Chest")
+                            ));
+                            equipment.setLeggings(Main.plugin.getConfig().getItemStack(
+                                    "ItemTable." + Main.plugin.getConfig().getString(
+                                            "MobTable.List." + i + ".Leg")
+                            ));
+                            equipment.setBoots(Main.plugin.getConfig().getItemStack(
+                                    "ItemTable." + Main.plugin.getConfig().getString(
+                                            "MobTable.List." + i + ".Foot")
+                            ));
+
+                        }
+
                     }
 
                 }
